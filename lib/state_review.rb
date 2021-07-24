e71b4c81bd949d1c6b16d85047f37fcff350fc28
Melissa Vogelgesangcat ~/.ssh/id_rsa.pubssh-keygenssh-rsassh-keygencat ~/.ssh/id_rsa.pubcat ~/.ssh/id_rsa.pub <mvogelgesang01@gmail.com>
2021-07-24 16:13:20 -0500

    require environment

---
+++ lib/state_review.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "state_review/version"
+
+module StateReview
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/state_review/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module StateReview
+  VERSION = "0.1.0"
+end
